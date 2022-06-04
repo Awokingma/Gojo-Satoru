c34450601130be2c85a7d60f6ab6a2ba79811c84
Awokingma <100766319+Awokingma@users.noreply.github.com>
2022-06-04 15:39:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94710999999']
+global.premium = ['94710999999']
+global.ownernomer = '*🔥HARSHA MADHUSHANKE♥️*'
+global.ownername = '🐦ESRO KING R'
+global.botname = 'HARSHA MADHUSHANKA'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
